5efdae9ac0840373cc1df5eeb72d94483189465c
antonioMg1979 <77509442+antonioMg1979@users.noreply.github.com>
2021-06-17 11:13:56 +0200

    17 06 2021
    PAra terminar la aplicación según diseño, una vez se conocen las unidades de almacenamiento, se debe indicar el Path donde se quiere revisar los archivos que pueden estar dublicado.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace AppOrden
     {
        public static void Main()
         {
+            String cadenaPathDrive = "";
             DriveInfo[] allDrives = DriveInfo.GetDrives();
 
             foreach (DriveInfo d in allDrives)
@@ -19,17 +20,29 @@ namespace AppOrden
                 Console.WriteLine("  Drive type: {0}", d.DriveType);
                 if (d.IsReady == true)
                 {
-                    Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
-                    Console.WriteLine("  File system: {0}", d.DriveFormat);
+                    Console.WriteLine("  Etiqueta de volumen: {0}", d.VolumeLabel);
+                    Console.WriteLine("  Sistema de archivos: {0}", d.DriveFormat);
                     Console.WriteLine(
-                        "  Available space to current user:{0, 15} bytes",
+                        "  Espacio disponible para la usuaria actual:{0, 15} bytes",
                         d.AvailableFreeSpace);
 
-                    Console.WriteLine("  Total available space:          {0, 15} bytes",d.TotalFreeSpace);
+                    Console.WriteLine(" Espacio total disponible:          {0, 15} bytes",d.TotalFreeSpace);
 
-                    Console.WriteLine("  Total size of drive:            {0, 15} bytes ", d.TotalSize);
+                    Console.WriteLine("  Tamaño total de la unidad:            {0, 15} bytes ", d.TotalSize);
                 }
             }
+
+            Console.WriteLine(" ");
+            Console.Write ("Indique el Drive deseado para listar su contenido,debe escribir la letra : ");
+            cadenaPathDrive = Console.ReadLine();
+            cadenaPathDrive += ":\\" ;
+            Console.WriteLine("");
+            Console.WriteLine(cadenaPathDrive);
+
+
+            //
+
         }
+
     }
  }      
